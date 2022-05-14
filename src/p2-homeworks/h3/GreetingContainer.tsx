efcd2f8a3013b8b4995b1bb36a1959237d7a0a23
m4nchkin <m4nchkin5@gmail.com>
2022-05-14 22:51:44 +0300

    Initial commit

--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -4,28 +4,35 @@ import {UserType} from "./HW3";
 
 type GreetingContainerPropsType = {
     users: Array<UserType> // need to fix any
-    addUserCallback: (name: string)=>void // need to fix any
+    addUserCallback: (name: string) => void // need to fix any
 }
 
 
 // более простой и понятный для новичков
-// function GreetingContainer(props: GreetingPropsType) {
+function GreetingContainer(props: GreetingContainerPropsType) {
 
 // более современный и удобный для про :)
 // уровень локальной логики
-const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+// const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
+    const [name, setName] = useState<string>('') // need to fix any
+    const [error, setError] = useState<string | null>('') // need to fix any
 
-    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
-        setName('') // need to fix
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => {// need to fix any
+        setError(null)
+        setName(e.currentTarget.value) // need to fix
     }
     const addUser = () => {
-        alert(`Hello  !`) // need to fix
+        if (name.trim() !== '') {
+            props.addUserCallback(name)
+            alert('Hello ' + name.trim() + ' !') // need to fix
+            setName('')
+        } else {
+            setError('Name is required!')
+        }
     }
 
-    const totalUsers = 0 // need to fix
-
+    const totalUsers = props.users.length
+    // console.log(totalUsers.length)
     return (
         <Greeting
             name={name}
--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -1,21 +1,21 @@
 import React, {ChangeEvent, useState} from 'react'
 import GreetingContainer from './GreetingContainer'
+import {v1} from "uuid";
 
 // types
 export type UserType = {
-    _id: number // need to fix any
+    _id: string // need to fix any
     name: string // need to fix any
 }
 
 // уровень работы с глобальными данными
 function HW3() {
-    const [users, setUsers] = useState<Array<UserType>>([
-        {_id:1,name:'Viktor'},
-        {_id:2,name:'Oleg'}
-    ]) // need to fix any
+    const [users, setUsers] = useState<Array<UserType>>([]) // need to fix any
 
-    const addUserCallback = (name: string) => { // need to fix any
-        setUsers()
+    const addUserCallback = (name: string) => {// need to fix any
+        let newName = {_id: v1(), name: name}
+
+        setUsers([newName]) //need to fix
     }
 
     return (
