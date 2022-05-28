1164ec381adbda03f46ba4551494de612f1044ca
m4nchkin <m4nchkin5@gmail.com>
2022-05-28 23:55:32 +0300

    Initial Commit

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Pages";
+import s from './pages/style.module.css'
+
+type LinksPropsType = {
+    collapsed: boolean
+    onClick: () => void
+}
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
 
+    const [collapsed, setCollapsed] = useState(false)
+
+    const onClickHandler = () => {
+        setCollapsed(!collapsed)
+    }
+
+
+    const Links = (props: LinksPropsType) => {
+        return <>
+            <h2 className={s.dropdown}><span>LINKS</span></h2>
+            <div className={s.dropdownContent}>
+                <NavLink to={PATH.PRE_JUNIOR}> pre-junior</NavLink>
+                <NavLink to={PATH.JUNIOR}> junior</NavLink>
+                <NavLink to={PATH.JUNIOR_PLUS}> junior-plus</NavLink>
+            </div>
+            {/*{props.collapsed && <Body/>}*/}
+    </>
+    }
+
+    return (
+
+        <div>
+            <Links collapsed={collapsed} onClick={onClickHandler}/>
         </div>
     )
 }
--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+// import {Redirect, Route, Switch} from "react-router-dom";
+import Error404 from "./pages/Error404";
+import {PreJunior} from "./pages/PreJunior";
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
@@ -8,17 +11,17 @@ export const PATH = {
 function Routes() {
     return (
         <div>
-            {/*Switch выбирает первый подходящий роут*/}
+            {/*/!*Switch выбирает первый подходящий роут*!/*/}
             {/*<Switch>*/}
 
-            {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
-            {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
+            {/*/!*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*!/*/}
+            {/*/!*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*!/*/}
             {/*<Route path={'/'} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>*/}
 
             {/*<Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>*/}
-                // add routes
+            {/*    // add routes*/}
 
-            {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
+            {/*/!*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*!/*/}
             {/*<Route render={() => <Error404/>}/>*/}
 
             {/*</Switch>*/}
